3bf523daa0204c96ef6f2ad2ceb70dd8953b09ea
yosef moshe <yosef moshe>
2020-03-23 17:20:07 +0200

    fix to aviad sheets func

--- home/views.py
+++ home/views.py
@@ -213,6 +213,30 @@ def check_status(text):
     else:
         return '3'
 
+def aviad_sheets():
+    # based on https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
+    # read that file for how to generate the creds and how to use gspread to read and write to the spreadsheet
+
+    # use creds to create a client to interact with the Google Drive API
+    scopes = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+
+    json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
+
+    creds_dict = json.loads(json_creds)
+    creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
+    client = gspread.authorize(creds)
+
+    # Find a workbook by url
+    spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1iKqFETeWoz1qj0qrp1GwZ2djyhIzJ6Gr3VrTY9jLU04/edit#gid=1164342576")
+    sheet = spreadsheet.sheet1
+
+    # Extract and print all of the values
+    rows = sheet.get_all_records()
+    print(rows)
+
+
 
 @csrf_exempt
 def update_sheets(request):
@@ -242,6 +266,8 @@ def update_sheets(request):
         # fb.status = check_status(str(status))
         # fb.save()
         return render(request, 'home/list.html')
+    else:
+        aviad_sheets()
 
 
 @csrf_exempt
@@ -258,28 +284,6 @@ def check_update(request):
             return JsonResponse({'success': False, 'status': '3'})
 
 
-def aviad_sheets():
-    # based on https://www.twilio.com/blog/2017/02/an-easy-way-to-read-and-write-to-a-google-spreadsheet-in-python.html
-    # read that file for how to generate the creds and how to use gspread to read and write to the spreadsheet
-
-    # use creds to create a client to interact with the Google Drive API
-    scopes = ['https://spreadsheets.google.com/feeds',
-             'https://www.googleapis.com/auth/drive']
-
-    json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
-
-    creds_dict = json.loads(json_creds)
-    creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
-    client = gspread.authorize(creds)
-
-    # Find a workbook by url
-    spreadsheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1iKqFETeWoz1qj0qrp1GwZ2djyhIzJ6Gr3VrTY9jLU04/edit#gid=1164342576")
-    sheet = spreadsheet.sheet1
-
-    # Extract and print all of the values
-    rows = sheet.get_all_records()
-    print(rows)
 
 @csrf_exempt
 def action_check(request):
